2a12c4832df940c411f3f78f903a5921e520b267
MrCsabaToth <csaba.toth.us@gmail.com>
2019-12-25 13:20:30 -0800

    Begin breaking out each digit into a separate UI element

--- nixie_clock/lib/nixie_clock.dart
+++ nixie_clock/lib/nixie_clock.dart
@@ -97,16 +97,29 @@ class _NixieClockState extends State<NixieClock> {
         ? _lightTheme
         : _darkTheme;
     final timeFormat = widget.model.is24HourFormat ? 'HH:mm:ss' : 'hh:mm:ss';
-    final time = DateFormat(timeFormat).format(_dateTime);
+    final timeFull = DateFormat(timeFormat).format(_dateTime);
+    final timeParts = timeFull.split(':');
+    final hours = timeParts[0];
+    final hourDigit1 = hours.length > 1 ? hours[0] : '';
+    final hourDigit2 = hours[hours.length > 1 ? 1 : 0];
+    final minutes = timeParts[1];
+    final minuteDigit1 = minutes[0];
+    final minuteDigit2 = minutes[1];
+    final seconds = timeParts[2];
+    final secondDigit1 = seconds[0];
+    final secondDigit2 = seconds[1];
+
     final fontSize = MediaQuery.of(context).size.width / 4.5;
     final offset = fontSize / 7;
+    final widgetWidth = fontSize / 1.6;
+    final colonWidth = fontSize / 4;
     final defaultStyle = TextStyle(
       color: colors[_Element.text],
       fontFamily: 'TTChocolates',
       fontSize: fontSize,
       shadows: [
         Shadow(
-          blurRadius: 10,
+          blurRadius: 20,
           color: colors[_Element.shadow],
           offset: Offset(0, 0),
         ),
@@ -120,8 +133,58 @@ class _NixieClockState extends State<NixieClock> {
           style: defaultStyle,
           child: Stack(
             children: <Widget>[
-              Positioned(left: offset, top: offset, child: Text(time)),
-//              Positioned(right: offset, bottom: offset, child: Text(minute)),
+              Positioned(
+                  left: offset,
+                  top: offset,
+                  child: Center(
+                      child: Text(hourDigit1),
+                  ),
+              ),
+              Positioned(
+                  left: offset + widgetWidth,
+                  top: offset,
+                  child: Center(
+                      child: Text(hourDigit2),
+                  ),
+              ),
+              Positioned(
+                  left: offset + 2 * widgetWidth,
+                  top: offset,
+                  child: Center(child: Text(':')),
+              ),
+              Positioned(
+                  left: offset + 2 * widgetWidth + colonWidth,
+                  top: offset,
+                  child: Center(
+                      child: Text(minuteDigit1),
+                  ),
+              ),
+              Positioned(
+                  left: offset + 3 * widgetWidth + colonWidth,
+                  top: offset,
+                  child: Center(
+                      child: Text(minuteDigit2),
+                  ),
+              ),
+              Positioned(
+                  left: offset + 4 * widgetWidth + colonWidth,
+                  top: offset,
+                  child: Center(child: Text(':')),
+              ),
+              Positioned(
+                  left: offset + 4 * widgetWidth + 2 * colonWidth,
+                  top: offset,
+                  child: Center(
+                      child: Text(secondDigit1),
+                  ),
+              ),
+              Positioned(
+                  left: offset + 5 * widgetWidth + 2 * colonWidth,
+                  top: offset,
+                  child: Center(
+                      child: Text(secondDigit2),
+                  ),
+              ),
             ],
           ),
         ),
