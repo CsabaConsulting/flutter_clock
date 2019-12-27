4e03686fe3eb2ae389e1d73470b7571334e3b734
MrCsabaToth <csaba.toth.us@gmail.com>
2019-12-27 00:22:16 -0800

    Looks like we won't use the hexagonal grid widget...

--- nixie_clock/lib/nixie_tube.dart
+++ nixie_clock/lib/nixie_tube.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:hexagonal_grid/hexagonal_grid.dart';
-import 'package:hexagonal_grid_widget/hex_grid_child.dart';
-import 'package:hexagonal_grid_widget/hex_grid_context.dart';
-import 'package:hexagonal_grid_widget/hex_grid_widget.dart';
 
 
 class NixieTubeWidget extends StatelessWidget {
