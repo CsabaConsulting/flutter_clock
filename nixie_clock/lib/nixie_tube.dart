ca1f3a0169a78bac7aba3f70ed9cfb82b2b6e572
MrCsabaToth <csaba.toth.us@gmail.com>
2019-12-29 12:20:00 -0800

    Adding stack of off digits into the nixie tube

--- nixie_clock/lib/nixie_tube.dart
+++ nixie_clock/lib/nixie_tube.dart
@@ -2,30 +2,114 @@ import 'package:flutter/material.dart';
 
 
 class NixieTubeWidget extends StatelessWidget {
-  NixieTubeWidget({this.digit, this.width, this.style, this.gridPainter});
+  NixieTubeWidget({
+    this.character,
+    this.width,
+    this.onStyle,
+    this.offStyle,
+    this.gridPainter
+  });
   /// Digit to display
-  final String digit;
+  final String character;
   final double width;
-  final TextStyle style;
+  final TextStyle onStyle;
+  final TextStyle offStyle;
   final CustomPainter gridPainter;
+
+  Widget buildDigits(BuildContext context) {
+    // final digitOrder = '6574839201';
+    return Stack(
+      children: <Widget>[
+        Center(
+          child: DefaultTextStyle(
+            style: character == '6' ? onStyle : offStyle,
+            child: Text('6'),
+          )
+        ),
+        Center(
+          child: DefaultTextStyle(
+            style: character == '5' ? onStyle : offStyle,
+            child: Text('5'),
+          ),
+        ),
+        Center(
+          child: DefaultTextStyle(
+            style: character == '7' ? onStyle : offStyle,
+            child: Text('7'),
+          ),
+        ),
+        Center(
+          child: DefaultTextStyle(
+            style: character == '4' ? onStyle : offStyle,
+            child: Text('4'),
+          ),
+        ),
+        Center(
+          child: DefaultTextStyle(
+            style: character == '8' ? onStyle : offStyle,
+            child: Text('8'),
+          ),
+        ),
+        Center(
+          child: DefaultTextStyle(
+            style: character == '3' ? onStyle : offStyle,
+            child: Text('3'),
+          ),
+        ),
+        Center(
+          child: DefaultTextStyle(
+            style: character == '9' ? onStyle : offStyle,
+            child: Text('9'),
+          ),
+        ),
+        Center(
+          child: DefaultTextStyle(
+            style: character == '2' ? onStyle : offStyle,
+            child: Text('2'),
+          ),
+        ),
+        Center(
+          child: DefaultTextStyle(
+            style: character == '0' ? onStyle : offStyle,
+            child: Text('0'),
+          ),
+        ),
+        Center(
+          child: DefaultTextStyle(
+            style: character == '1' ? onStyle : offStyle,
+            child: Text('I'),
+          ),
+        ),
+//        CustomPaint(
+//          foregroundPainter: gridPainter,
+//        ),
+      ],
+    );
+  }
+
+  Widget buildOther(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Center(
+            child: DefaultTextStyle(
+              style: onStyle,
+              child: Text(character),
+            )
+        ),
+//        CustomPaint(
+//          foregroundPainter: gridPainter,
+//        ),
+      ],
+    );
+  }
+
+  bool isDigit(String s, int idx) => (s.codeUnitAt(idx) ^ 0x30) <= 9;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: width,
-      child: Center(
-        child: Stack(
-          children: <Widget>[
-            DefaultTextStyle(
-              style: style,
-              child: Text(digit == '1' ? 'I' : digit)
-            ),
-            CustomPaint(
-              painter: gridPainter,
-            ),
-          ],
-        ),
-      ),
+      child: isDigit(character, 0) ? buildDigits(context) : buildOther(context)
     );
   }
 }
