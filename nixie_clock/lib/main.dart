70cedbd56c73654a48164a52353567bed4bee338
MrCsabaToth <csaba.toth.us@gmail.com>
2020-01-19 20:16:17 -0800

    Remove comments which are not needed any more

--- nixie_clock/lib/main.dart
+++ nixie_clock/lib/main.dart
@@ -22,15 +22,9 @@ void main() {
     debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
   }
 
-  // This creates a clock that enables you to customize it.
-  //
   // The [ClockCustomizer] takes in a [ClockBuilder] that consists of:
   //  - A clock widget (in this case, [NixieClock])
   //  - A model (provided to you by [ClockModel])
-  // For more information, see the flutter_clock_helper package.
-  //
-  // Your job is to edit [NixieClock], or replace it with your
-  // own clock widget. (Look in nixie_vfd_clock.dart for more details!)
   runApp(ClockCustomizer((ClockModel model) =>
       NixieVFDClock(model: model, child: NixieVFDClockWidget())));
 }
