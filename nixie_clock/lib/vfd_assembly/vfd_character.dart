94ca43b83925beea28ece86444a1e307777ad2e5
MrCsabaToth <csaba.toth.us@gmail.com>
2020-01-20 22:12:50 -0800

    Removing unused arguments + dartfmt reformatting

--- nixie_clock/lib/vfd_assembly/vfd_character.dart
+++ nixie_clock/lib/vfd_assembly/vfd_character.dart
@@ -1,24 +1,15 @@
 import 'package:flutter/material.dart';
 
-import '../color_configuration.dart';
 import 'vfd_painter.dart';
 
 class VFDCharacter extends StatelessWidget {
   VFDCharacter({
-    this.colorSet,
     this.character,
-    this.charSize,
-    this.charMargin,
-    this.pixelSize,
     this.backgroundPainter,
     this.foregroundPainter,
   });
 
-  final Map<ColorSelector, Color> colorSet;
   final String character;
-  final Size charSize;
-  final Size charMargin;
-  final Size pixelSize;
   final VFDPainter backgroundPainter;
   final VFDPainter foregroundPainter;
 
--- nixie_clock/lib/vfd_assembly/vfd_line.dart
+++ nixie_clock/lib/vfd_assembly/vfd_line.dart
@@ -1,46 +1,32 @@
 import 'package:flutter/material.dart';
 
-import '../color_configuration.dart';
 import 'vfd_character.dart';
 import 'vfd_painter.dart';
 
 class VFDLine extends StatelessWidget {
   VFDLine({
-    this.colorSet,
     this.text,
     this.maxChars,
-    this.charSize,
-    this.charMargin,
-    this.pixelSize,
     this.backgroundPainter,
     this.foregroundPainter,
   });
 
-  final Map<ColorSelector, Color> colorSet;
   final String text;
   final int maxChars;
-  final Size charSize;
-  final Size charMargin;
-  final Size pixelSize;
   final VFDPainter backgroundPainter;
   final VFDPainter foregroundPainter;
 
   @override
   Widget build(BuildContext context) {
     final List<Widget> vfdCharacters = [];
-    text.padRight(maxChars, ' ').split('').forEach((character) =>
-      vfdCharacters.add(
-        VFDCharacter(
-          colorSet: colorSet,
-          character: character,
-          charSize: charSize,
-          charMargin: charMargin,
-          pixelSize: pixelSize,
-          backgroundPainter: backgroundPainter,
-          foregroundPainter: foregroundPainter,
-        )
-      )
-    );
+    text
+        .padRight(maxChars, ' ')
+        .split('')
+        .forEach((character) => vfdCharacters.add(VFDCharacter(
+              character: character,
+              backgroundPainter: backgroundPainter,
+              foregroundPainter: foregroundPainter,
+            )));
 
     return Row(
       mainAxisSize: MainAxisSize.min,
